5354af50c03bfa473a4060bd29e48aefd76be084
banseok <seok6711@naver.com>
2015-01-30 12:04:19 +0900

    Edit 2

--- banseokExam1/src/banseokExam1/Test2.java
+++ banseokExam1/src/banseokExam1/Test2.java
@@ -2,8 +2,8 @@ package banseokExam1;
 
 public class Test2 {
 	public static void main(String[] args){
-		System.out.println("Git 테스트 1");
-		System.out.println("Git 테스트 2 Add");
+		System.out.println("Git 테스트 1 edit 2");
+		System.out.println("Git 테스트 2 edit 2");
 	}
 
 }
